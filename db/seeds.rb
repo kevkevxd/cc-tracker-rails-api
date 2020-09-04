10f2a11210d9d24732acb4687fc6c175fc6ca942
victorscholz <vgscholz@gmail.com>
2020-09-04 14:47:59 -0400

    initial commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require_relative '../config/environment'
+require 'rest-client'
+require 'json'
+require 'pry'
+
+
