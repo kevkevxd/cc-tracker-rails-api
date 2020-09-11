3bb9f5e99b34f0fab9a9dce5c6f7f3e223788cea
victorscholz <vgscholz@gmail.com>
2020-09-11 17:20:16 -0400

    join model clean up

--- app/controllers/user_credit_cards_controller.rb
+++ app/controllers/user_credit_cards_controller.rb
@@ -4,4 +4,21 @@ class UserCreditCardsController < ApplicationController
         user_credit_cards = UserCreditCard.all
         render json: user_credit_cards
     end
+
+    def show
+        user_credit_card = find_user_credit_card
+        render json: user_credit_card
+    end
+
+
+    def destroy
+        user_credit_card = user_credit_card.destroy
+        render json: user_credit_card
+    end
+
+    private
+    def find_user_credit_card
+        user_credit_card = UserCreditCard.find(params[:id])
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,4 @@ Rails.application.routes.draw do
   resources :user_credit_cards
   resources :credit_cards
   resources :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
